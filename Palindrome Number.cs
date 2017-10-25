8038cc325c0a2761bebc909d35fa3dd0d02203e7
Kody C. Kendall <kodyckendall@gmail.com>
2017-10-25 08:40:40 -0600

    Update Palindrome Number.cs

--- Palindrome Number.cs
+++ Palindrome Number.cs
@@ -5,39 +5,12 @@ public class Solution
         
         string stringNumber = x.ToString();
         bool evenDigits = stringNumber.Length % 2 == 0; //If the palindrome is an odd number of digits, we ignore the center digit since it's irrelevant 
-        string firstHalf = "";
+        string firstHalf = stringNumber.Substring(0, stringNumber.Length/2);
         string secondHalf = "";
+        
+        if (evenDigits) secondHalf = stringNumber.Substring(stringNumber.Length/2); //If even, get second half
+        else secondHalf = stringNumber.Substring(stringNumber.Length/2+1);//If odd, get second half excluding middle-most digit
             
-        for (int index = 0; index < stringNumber.Length/2; index++)//Scan through first half of integer
-            firstHalf += stringNumber[index];
-        
-        if (evenDigits) //If it's an even number of digits, we check to length/2 (inclusive)  
-        {
-            for (int index = stringNumber.Length/2; index < stringNumber.Length; index++)
-                secondHalf += stringNumber[index];
-        }
-        
-        else 
-        {
-            for (int index = stringNumber.Length/2 + 1; index < stringNumber.Length; index++) //If it's an odd num of digits, we check to length/2 + 1 (ignoring the middle-most digit)
-                   secondHalf += stringNumber[index];
-        }
-        
-        //Check them against themselves backwards
-        bool palSoFar = true;
-        for (int index = 0; index < firstHalf.Length; index++)
-        {
-            int complementIndex = firstHalf.Length - 1 - index;
-            
-            if (firstHalf[index] == secondHalf[complementIndex])//Checks the half-strings against themselves in an inverse fashion -- "10" "01" would check if index[0] and index[3] are the same, because that's the correct corresponding integer
-                palSoFar = true;
-            else
-            {
-                palSoFar = false; 
-                break;
-            }
-        }
-        
         return AreTwoHalvesPalindrome(firstHalf, secondHalf);
     }
     
@@ -48,7 +21,7 @@ public class Solution
         
         for (int index = 0; index < half1.Length; index++)
         {
-            int complementIndex = firstHalf.Length - 1 - index;
+            int complementIndex = half1.Length - 1 - index;
             
             if (half1[index] == half2[complementIndex])//Checks the half-strings against themselves in an inverse fashion -- "10" "01" would check if index[0] and index[3] are the same, because that's the correct corresponding integer
                 palSoFar = true;
